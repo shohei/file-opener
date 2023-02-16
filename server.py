c88cde1d02baeaeb512603c909dd9fa424f74650
Shohei Aoki <shoaok@gmail.com>
2023-02-16 05:56:04 +0300

    add kindle open function

--- server.py
+++ server.py
@@ -6,11 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     pdf = request.args.get('pdf')
-    if pdf==None:
-        pdf = ""
-    pdf = pdf.replace("\s"," ")
-    cmd = "open " + "'"+pdf+"'"
-    os.system(cmd)
+    kindle = request.args.get('kindle')
+    if pdf!=None:
+        pdf = pdf.replace("\s"," ")
+        cmd = "open " + "'"+pdf+"'"
+        os.system(cmd)
+    elif kindle!=None:
+        asin = kindle
+        basecmd = 'kindle://book?action=open&asin=' + asin
+        cmd = "open " + "'"+basecmd+"'"
+        app.logger.info(cmd)
+        os.system(cmd)
     return "" 
 
 if __name__ == "__main__":
